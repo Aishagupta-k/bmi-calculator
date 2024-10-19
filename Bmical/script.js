24d48abdb148249b466351bc95b8c67cf0da2d39
Aishagupta-k <aishuukgupta@gmail.com>
2024-10-19 04:13:56 -0700

    Add files via upload

---
+++ Bmical/script.js
@@ -0,0 +1,20 @@
+let form = document.querySelector('form');
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    const weight = parseInt(document.querySelector('#weigth').value);
+    const height = parseInt(document.querySelector('#height').value);
+    const result =  document.querySelector('#results');
+    if(height=== '' || height<0 || isNaN(height)){
+        result.innerHTML= `please enter correct height ${height}`
+
+    }else if(weight=== '' || weight<=0 ||  isNaN(weight)){
+        result.innerHTML= `please enter correct weight ${weight}`;
+
+    }else{
+        const BMI = (weight / ((height * height) /10000)).toFixed(2);
+        result.innerHTML=BMI
+    }
+
+
+})
